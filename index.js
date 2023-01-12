c851b319c1e56baf0d5867c43369d343e6ea38a0
marc hudson <marchudson2601@gmail.com>
2023-01-12 23:00:13 +0000

    added function to get questions and answers and render elemet to the page

--- index.js
+++ index.js
@@ -1,29 +1,95 @@
-let scores = document.querySelector("#scores")
+// let scores = document.querySelector("#scores")
 let timer = document.querySelector("#time");
 let startButt = document.querySelector("#start");
-let questionsAnswer = document.querySelector("#choices");
+let startScreen = document.querySelector("#start-screen")
+let questionAnswers = document.querySelector("#choices");
 let questions = document.querySelector("#questions");
 let questionTitel = document.querySelector("#question-title");
-let endScreen = document.querySelector("#end-screen");
-let finalScore = document.querySelector("#final-score");
-let initials = document.querySelector("#initials");
-let submitInitials = document.querySelector("#submit")
-let userFeedback = document.querySelector("#feedback");
+// let endScreen = document.querySelector("#end-screen");
+// let finalScore = document.querySelector("#final-score");
+// let initials = document.querySelector("#initials");
+// let submitInitials = document.querySelector("#submit")
+// let userFeedback = document.querySelector("#feedback");
+
+
+
+
+let questionsArray = [
+    {
+        question: "whats the corect way to declare a variable",
+        answer: 0,
+        choises: [
+            "let x = 4",
+            "let 4 = x",
+            "let const 4 = var",
+            "4 = x let",
+        ]
+    },
+    {
+        question: "whats the example of a function expresion",
+        answer: 0,
+        choises: [
+            "let x = 4",
+            "let 4 = x",
+            "let const 4 = var",
+            "4 = x let",
+        ]
+    }
+]
 
 // global variables
-timercount = 100;
-score = 0;
-q = 0;
+let timercount = 100;
+let scores = 0;
+let q = 0;
 
 
 
-//timerfunction
+// button click event to start quiz
+startButt.addEventListener("click", () => {
+    startQuizz()
+})
 
-let activetime = setInterval(() => {
-    timer.textContent = timercount
-    timercount = timercount - 1
-    if ( timercount <= -1 ) {
-        clearInterval(activetime);
-        alert("done")
+
+
+function startQuizz() {
+    // resets all global variables
+    timercount = 100;
+    scores = 0;
+    q = 0;
+    startScreen.className = "hide";
+
+    getNextQuestion()
+    //timerfunction
+    let activetime = setInterval(() => {
+        timer.textContent = timercount
+        timercount = timercount - 1
+        if (timercount <= -1) {
+            clearInterval(activetime);
+
+        }
+    }, 1000)
+
+}
+function getNextQuestion() {
+    if (timercount <= 0) {
+        console.log("pooooo")
     }
-}, 1000)
+    //reset questions state
+    questionAnswers.innerHTML = ""
+    questions.className = "start"
+    // add question to page
+    questionTitel.textContent = questionsArray[q].question
+    questionsArray[q].choises.forEach(element => {
+    let button = document.createElement("button")
+    button.textContent = element
+    questionAnswers.appendChild(button)
+    });
+}
+
+
+
+
+
+
+
+
